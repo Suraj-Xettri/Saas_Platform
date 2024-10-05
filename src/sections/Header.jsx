c26938d6fe27fcb0b7c9f8d71ae5b8dc9a8f7c05
Suraj Thapa <surajthapart@gmail.com>
2024-10-05 11:12:54 +0545

    header section completed with responsiveness

--- src/sections/Header.jsx
+++ src/sections/Header.jsx
@@ -1,5 +1,7 @@
-import React from "react";
+import clsx from "clsx";
+import React, { useState } from "react";
 import { Link as LinkScroll } from "react-scroll";
+
 
 const NavLink = ({ title }) => (
   <LinkScroll className="base-bold text-p4 uppercase transition-colors duration-500 cursor-pointer hover:text-p1 max-lg:my-4 max-lg:h5">
@@ -8,6 +10,7 @@ const NavLink = ({ title }) => (
 );
 
 const Header = () => {
+  const [isOpen, setIsOpen] = useState(false);
   return (
     <header className="fixed top-0 left-0 z-50 w-full py-7">
       <div className="container flex h-14 items-center max-lg:px-5">
@@ -15,8 +18,13 @@ const Header = () => {
           <img src="/images/xora.svg" width={115} height={55} alt="Logo" />
         </a>
 
-        <div className="w-full max-lg:fixed max-lg:top-0 max-lg:left-0 max-lg:w-full max-lg:bg-s2 max-lg:opacity-0">
-          <div className="w-full max-lg:relative max-lg:flex max-lg:flex-col max-lg:min-h-screen max-lg:p-6 max-lg:overflow-hidden sidebar-before max-md:px-4 ">
+        <div
+          className={clsx(
+            "w-full max-lg:fixed max-lg:top-0 max-lg:left-0 max-lg:w-full max-lg:bg-s2",
+            isOpen ? "max-lg:opacity-100" : "max-lg:opacity-0 max-lg:invisible"
+          )}
+        >
+          <div className="w-full max-lg:relative max-lg:flex max-lg:flex-col max-lg:min-h-screen max-lg:p-4 max-lg:justify-center max-lg:overflow-hidden max-md:px-1 ">
             <nav>
               <ul className="flex max-lg:block max-lg:px-12">
                 <li className="nav-li">
@@ -28,7 +36,15 @@ const Header = () => {
                 </li>
 
                 <li className="nav-logo">
-                  <LinkScroll>
+                  <LinkScroll
+                    to="hero"
+                    offset={-100}
+                    spy
+                    smooth
+                    className={clsx(
+                      "max-lg:hidden transition-transform duration-500 cursor-pointer "
+                    )}
+                  >
                     <img
                       src="/images/xora.svg"
                       width={160}
@@ -39,7 +55,7 @@ const Header = () => {
                 </li>
 
                 <li className="nav-li">
-                  <NavLink title="faq" />
+                  <NavLink title="faq"/>
 
                   <div className="dot"></div>
 
@@ -47,8 +63,32 @@ const Header = () => {
                 </li>
               </ul>
             </nav>
+
+            <div className="lg:hidden block absolute top-1/2 left-0 w-[960px] h-[380px] translate-x-[-290px] -translate-y-1/2 rotate-90">
+              <img
+                src="/images/bg-outlines.svg"
+                width={960}
+                height={180}
+                alt="outline"
+                className="relative z-2"
+              />
+              <img
+                src="/images/bg-outlines-fill.png"
+                width={960}
+                height={180}
+                alt="outline"
+                className="absolute insect-0 mix-blend-soft-light 5"
+              />
+            </div>
           </div>
         </div>
+
+        <button
+          className="lg:hidden z-2 size-10 border-2 border-s4/25 rounded-full flex justify-center items-center"
+          onClick={() => setIsOpen((p) => !p)}
+        >
+          <img src="images/magic.svg" alt="" />
+        </button>
       </div>
     </header>
   );
