b7ea07c3b8bc593427eda789981ea5aba8d51288
Suraj Thapa <surajthapart@gmail.com>
2024-10-06 12:34:02 +0545

    created hero section

---
+++ src/sections/Hero.jsx
@@ -0,0 +1,33 @@
+import React from "react";
+import { Element , Link as LinkScroll} from "react-scroll";
+
+const Hero = () => {
+  return (
+    <section className="relative pt-60 pb-40 max-lg:pt-50 max-lg:pb-36 max-md:pt-36 max-md:pb-32 border-2 border-amber-400">
+      <Element name="hero">
+        <div className="container">
+          <div className="relative z-2 max-w-512 max-lg:max-w-388">
+            <div className="caption small-2 uppercase text-p3">
+              Video Editing
+            </div>
+
+            <h1 className="mb-6 h1 uppercase text-p4 max-lg:mb-7 max-lg:h2 max-md:mb-4 max-md:text-5xl max-md:leading-12">
+              amazingly simple
+            </h1>
+
+            <p className="max-w-440 mb-14 body-1 max-md:mb-10">
+              We designed zorz AI Video editor to be an easy to use, quick to
+              learn, and suprisingly powerful.
+            </p>
+
+            <LinkScroll to="features" offset = {-100} spy smooth>
+                
+            </LinkScroll>
+          </div>
+        </div>
+      </Element>
+    </section>
+  );
+};
+
+export default Hero;
