2546f5fcff82be3cf6fb1ad363c36b2a45102dba
Suraj Thapa <surajthapart@gmail.com>
2024-10-08 06:05:09 +0545

    hero section completed

--- src/sections/Hero.jsx
+++ src/sections/Hero.jsx
@@ -4,7 +4,7 @@ import Button from "../components/Button";
 
 const Hero = () => {
   return (
-    <section className="relative pt-60 pb-40 max-lg:pt-50 max-lg:pb-36 max-md:pt-36 max-md:pb-32">
+    <section className="relative pt-52 pb-40 max-lg:pt-50 max-lg:pb-36 max-md:pt-36 max-md:pb-32">
       <Element name="hero">
         <div className="container">
           <div className="relative z-2 max-w-512 max-lg:max-w-388">
@@ -25,7 +25,7 @@ const Hero = () => {
                 <Button icon="/images/zap.svg">Try it now</Button>
             </LinkScroll>
           </div>
-          <div className="absolute -top-32 left-2/3">
+          <div className="absolute -top-32 left-[calc(50%-340px)] w-[1230px] pointer-events-none hero-img_res">
               <img src="/images/hero.png" alt="hero" className="size-1230 max-lg:h-auto left-[calc(50%-340px)] w-[1230px]"/>
           </div>
         </div>
