90a66b8ca239ac91376b90940a875f2e67740f7d
Suraj Thapa <surajthapart@gmail.com>
2024-10-08 06:53:26 +0545

    made changes in header and made it scroll according to nav

--- src/sections/Header.jsx
+++ src/sections/Header.jsx
@@ -1,18 +1,44 @@
 import clsx from "clsx";
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { Link as LinkScroll } from "react-scroll";
-
-
-const NavLink = ({ title }) => (
-  <LinkScroll className="base-bold text-p4 uppercase transition-colors duration-500 cursor-pointer hover:text-p1 max-lg:my-4 max-lg:h5">
-    {title}
-  </LinkScroll>
-);
 
 const Header = () => {
   const [isOpen, setIsOpen] = useState(false);
+  const [hasScrolled, setHasScrolled] = useState(true);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      setHasScrolled(window.scrollY > 32);
+    };
+
+    window.addEventListener("scroll", handleScroll);
+
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, []);
+
+  const NavLink = ({ title }) => (
+    <LinkScroll
+      onClick={() => setIsOpen(false)}
+      to={title}
+      offset={-100}
+      spy
+      smooth
+      activeClass="nav-active"
+      className="base-bold text-p4 uppercase transition-colors duration-500 cursor-pointer hover:text-p1 max-lg:my-4 max-lg:h5"
+    >
+      {title}
+    </LinkScroll>
+  );
+
   return (
-    <header className="fixed top-0 left-0 z-50 w-full py-7">
+    <header
+      className={clsx(
+        "fixed top-0 left-0 z-50 w-full py-7 transition-all duration-500 max-lg:py-4",
+        hasScrolled && "py-2 bg-black-100 bg-blur-[8px] "
+      )}
+    >
       <div className="container flex h-14 items-center max-lg:px-5">
         <a className="lg:hidden flex-1 cursor-pointer z-2">
           <img src="/images/xora.svg" width={115} height={55} alt="Logo" />
@@ -38,7 +64,7 @@ const Header = () => {
                 <li className="nav-logo">
                   <LinkScroll
                     to="hero"
-                    offset={-100}
+                    offset={-250}
                     spy
                     smooth
                     className={clsx(
@@ -55,11 +81,9 @@ const Header = () => {
                 </li>
 
                 <li className="nav-li">
-                  <NavLink title="faq"/>
-
-                  <div className="dot"></div>
-
-                  <NavLink title="download" />
+                  <NavLink title="faq" />
+                  <div className="dot" />
+                  <NavLink title="pricing" />
                 </li>
               </ul>
             </nav>
@@ -68,16 +92,16 @@ const Header = () => {
               <img
                 src="/images/bg-outlines.svg"
                 width={960}
-                height={180}
+                height={380}
                 alt="outline"
                 className="relative z-2"
               />
               <img
                 src="/images/bg-outlines-fill.png"
                 width={960}
-                height={180}
+                height={380}
                 alt="outline"
-                className="absolute insect-0 mix-blend-soft-light 5"
+                className="absolute insect-0 mix-blend-soft-light opacity-5"
               />
             </div>
           </div>
